1dabc03d0a48ea92adf5599859ce4d076479432d
Madhav Nandan <mnandan06@gmail.com>
2016-04-14 06:17:48 -0700

    Program 20
    Not properlycompiled

---
+++ DumbKit.java/DumbKit.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class DumbKit here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+ class DumbKit
+{
+    boolean topHat = true;
+    boolean  snare = true;
+    
+    void playTopHat() {
+        System.out.println("ding ding da-ding");
+    }
+}
+
+
