09043057b1dcc0c01bb86ffdf2d1cd685366d706
Madhav Nandan <mnandan06@gmail.com>
2016-05-01 13:13:26 -0700

    Program 28
    Math.sqrt , Math.abs , Math.max are used

---
+++ MathPro.java/MathPro.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class MathPro here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MathPro
+{
+    public static void main(String[] args){
+    
+        int a = 0;
+        double b = 1.5;
+        
+        System.out.println(Math.sqrt(144) + " " + Math.abs(-6) + " " + Math.max(10,17));
+    }
+}
