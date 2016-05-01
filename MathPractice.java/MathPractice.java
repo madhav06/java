36173df6e048dc8372c3ca2ef788a1cbc55699cc
Madhav Nandan <mnandan06@gmail.com>
2016-05-01 12:59:49 -0700

    Program 27
    Math power function

---
+++ MathPractice.java/MathPractice.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class MathPractice here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MathPractice
+{
+    public static void main(String[] args){
+        int a = 1;
+        double b = 0;
+        
+        System.out.println((int)Math.pow(2,5));
+    }
+}
