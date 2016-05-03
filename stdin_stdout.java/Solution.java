588da419b24d5ad4bc17e9b9f9a23402dcbe532a
Madhav Nandan <mnandan06@gmail.com>
2016-05-03 15:01:10 -0700

    Program 30
    stdin stdout

---
+++ stdin_stdout.java/Solution.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            int x=sc.nextInt();
+            Double y=sc.nextDouble();sc.nextLine();
+            String s=sc.nextLine();
+            //Complete this code
+
+            System.out.println("String: "+s);
+            System.out.println("Double: "+y);
+            System.out.println("Int: "+x);
+    }
+}
+
