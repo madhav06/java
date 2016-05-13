fec73432fe6386d69328a97c1f3037d20138c572
Madhav Nandan <mnandan06@gmail.com>
2016-05-13 15:53:39 +0530

    Program 36
    bug fixed !!

--- Hobbits.java/Hobbits.java
+++ Hobbits.java/Hobbits.java
@@ -1,5 +1,5 @@
 
-public class Hobbits
+class Hobbits
 {
     
     
@@ -9,9 +9,9 @@ public class Hobbits
     public static void main(String[] args) {
     
         Hobbits [] h = new Hobbits[3];
-        int z = 0;
+        int z = -1;
         
-        while (z < 4) {
+        while (z < 2) {
             z = z + 1;
             h[z] = new Hobbits();
             h[z].name = "bilbo";
