50e8d30a1f57b4e306de2e1897717d540fd1807f
Madhav Nandan <mnandan06@gmail.com>
2016-08-05 15:23:02 +0530

    Program 43
    Duck.java ## we used constructor here ##

---
+++ Duck.java/Duck.java.java
@@ -0,0 +1,15 @@
+/* package codechef; // don't place package name! */
+
+public class Duck {
+    public Duck() {
+        System.out.println("Quack");
+    }
+}
+
+
+
+public class UseADuck {
+    public static void main (String[] args) {
+        Duck d = new Duck();
+    }
+}
