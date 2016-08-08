f857005ec47bab622ccfad1df47f76c44c653c6e
Madhav Nandan <mnandan06@gmail.com>
2016-08-08 14:07:27 +0530

    Program 55
    method overloading :
    
    1. By changing number of arguments
    
    2. By changing the data-types

---
+++ ObjectOrientedConcepts/src/objectorientedconcept/com/Calculation1.java
@@ -0,0 +1,17 @@
+package objectorientedconcept.com;
+
+class Calculation1 {
+	void sum(int a , int b) {
+		System.out.println(a+b);
+	}
+	void sum(int a , int b , int c) {
+		System.out.println(a+b+c);
+	}
+	public static void main(String[] args) {
+		Calculation1 obj = new Calculation1();
+		
+		obj.sum(20, 20);
+		obj.sum(10,10, 10);
+	}
+
+}
