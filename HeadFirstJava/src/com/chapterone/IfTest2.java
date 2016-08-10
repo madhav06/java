3637a147c59ab12e4f4fde4ee5c9c30de6b0114b
Madhav Nandan <mnandan06@gmail.com>
2016-08-10 17:47:39 +0530

    program 58
    Loopy 
    IfTEst2
    Shuffle1
    PoolPuzzleOne

---
+++ HeadFirstJava/src/com/chapterone/IfTest2.java
@@ -0,0 +1,18 @@
+package com.chapterone;
+
+//import sun.applet.Main;
+
+public class IfTest2 {
+	
+	public static void main(String[] args) {
+		int x = 2;
+		
+		if (x == 3) {
+			System.out.println("x must be 3 ");
+		}else {
+			System.out.println("x is NOT 3 ");
+		}
+		System.out.println("This runs no matter what");
+	}
+
+}
