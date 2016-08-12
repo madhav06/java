9ccffda5c867577b480f448f0d39468f7a401d71
Madhav Nandan <mnandan06@gmail.com>
2016-08-12 17:04:01 +0530

    Program 62
    chapter 3 codes ; on variables and using them

---
+++ HeadFirstJava1/src/com/chapterthree/Dog.java
@@ -0,0 +1,54 @@
+package com.chapterthree;
+
+ class Dog {
+	 
+	 String name;
+	 public static void main(String[] args) {
+		//make a Dog oblect and access it
+		 
+		 Dog dog1 = new Dog();
+		 dog1.bark();
+		 dog1.name = "Bart";
+		 
+		 //now make adog array
+		 
+		 Dog[] myDogs = new Dog[3];
+		 
+		 myDogs[0] = new Dog();
+		 myDogs[1] = new Dog();
+		 myDogs[2] = dog1;
+		 
+		 //now access the Dogs using the array
+		 //references
+		 
+		 myDogs[0].name = "Fred";
+		 myDogs[1].name = "Marge";
+		 
+		 //Hmm...what is myDogs[2] name ??
+		 
+		 System.out.println("last dog's name is  ");
+		 System.out.println(myDogs[2].name);
+		 
+		 //now loop through the array
+		 //and tell all dogs to bark
+		 
+		 
+		 int x = 0;
+		 while (x < myDogs.length) {
+			 myDogs[x].bark();
+			 x = x +1;
+		 }
+	 }
+		 
+	
+
+
+
+ 
+ public void bark() {
+	 
+	 System.out.println(name + "says Ruff!!");
+ }
+ public void eat() { }
+	 public void chaseCat() { }
+ }
