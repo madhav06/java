dce556bdb4e8a4d4c15bf3565c88eb8f1a590f1a
Madhav Nandan <mnandan06@gmail.com>
2016-08-16 18:33:37 +0530

    Program 65
    OOps concept Encapsulation

---
+++ OOP/src/com/encapsulation/Test3.java
@@ -0,0 +1,13 @@
+package com.encapsulation;
+
+public class Test3 {
+	
+	public static void main(String[] args) {
+		
+		Employee e = new Employee("MAddy" ,  27000);
+		e.setSalary(27000);
+		
+		System.out.println(e.getSalary());
+	}
+
+}
