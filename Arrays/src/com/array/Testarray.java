2e4380ba8872b55210ad53f6c8474cc6165dc03c
Madhav Nandan <mnandan06@gmail.com>
2016-08-17 16:50:34 +0530

    Program 68
    Arrays programs and concepts

---
+++ Arrays/src/com/array/Testarray.java
@@ -0,0 +1,19 @@
+package com.array;
+
+public class Testarray {
+	
+	public static void main(String[] args) {
+		
+		//int[] arr = new int[5];
+		
+		int[] ar = { 10 , 20 , 30 , 40 , 50};
+		
+		for(int i = 0; i < ar.length ; i++) 
+			
+			System.out.println(ar[i]);
+		
+		
+		
+	}
+
+}
