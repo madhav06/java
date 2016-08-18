ffdf11418d418ad9cd8fda935d51510b2e11a481
Madhav Nandan <mnandan06@gmail.com>
2016-08-18 19:25:40 +0530

    Program 73
    Array Print Console

---
+++ ArrayPrintConsole/src/com/arrayprintingtoconsole/ArrayPrintConsole.java
@@ -0,0 +1,21 @@
+package com.arrayprintingtoconsole;
+
+import java.util.Arrays;
+
+public class ArrayPrintConsole {
+	
+	public static void main(String[] args) {
+		
+		
+		int[] intArray = new int[] {1 , 2 , 3 , 4 , 5};
+		
+		System.out.println(Arrays.toString(intArray));
+		
+		
+		String[] stringArray = new String[] {"J" , "M" , "B"};
+		
+		System.out.println(Arrays.toString(stringArray));
+		
+	}
+
+}
