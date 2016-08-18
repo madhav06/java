ee00cdcaf545847298f36b5b8d6a5a8abb44c68a
Madhav Nandan <mnandan06@gmail.com>
2016-08-18 16:47:03 +0530

    Program 69
    Convert String to Integer

---
+++ StringToInt/src/com/stringtoint/StringToInt.java
@@ -0,0 +1,34 @@
+package com.stringtoint;
+
+//import java.util.Scanner;
+
+public class StringToInt {
+	
+	public static void main(String[] args) {
+		
+		//Scanner sc = new Scanner(System.in);
+		
+		/*String str1 = "12030";
+		
+		System.out.println("Enter a String ");
+		
+		str1 = sc.nextLine(); */
+		
+		int i = Integer.parseInt("12030");
+		
+		System.out.println("I'm integer "+i);
+		
+		double d = Double.parseDouble("2.332");
+		
+		System.out.println("I'm double "+d);
+		
+		float f = Float.parseFloat("2.33");
+		
+		System.out.println("I'm float "+f);
+				
+				
+		
+		
+	}
+
+}
