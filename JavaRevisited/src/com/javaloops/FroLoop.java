af1e4e4eda37831713cfaf9b8dcb96c83fd0afc8
Madhav Nandan <mnandan06@gmail.com>
2016-08-19 18:01:14 +0530

    Program 77
    Loops - For loops

---
+++ JavaRevisited/src/com/javaloops/FroLoop.java
@@ -0,0 +1,20 @@
+package com.javaloops;
+
+public class FroLoop {
+
+//}
+
+
+
+
+//public class ForLoop {
+	
+	public static void main(String[] args) {
+		
+		for(int i=1 ; i<100;i++){
+			
+			System.out.println(i);
+		}
+	}
+
+}
