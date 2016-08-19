f5e7504ff37e545624cc67c8a85ec69a3d4a8be3
Madhav Nandan <mnandan06@gmail.com>
2016-08-19 18:02:03 +0530

    Program 78
    Program loop

---
+++ JavaRevisited/src/com/javaloops/EvenNaturalNumber.java
@@ -0,0 +1,14 @@
+package com.javaloops;
+
+public class EvenNaturalNumber {
+	
+	public static void main(String[] args) {
+		
+		
+		for(int i=2; i <= 200; i += 2){
+			
+			System.out.println("100 Natural numbers are : "+i);
+		}
+	}
+
+}
