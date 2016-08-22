d68df489ee27ba4660177058629938ad20db8582
Madhav Nandan <mnandan06@gmail.com>
2016-08-22 16:35:44 +0530

    Program 81
    substring program

---
+++ JavaRevisited/src/com/javaloops/SubstringExample.java
@@ -0,0 +1,18 @@
+package com.javaloops;
+
+public class SubstringExample {
+
+//}
+
+
+//public class SubstringExample{  
+public static void main(String args[]){  
+String s1="keshav";  
+String s2 = "nandan";
+System.out.print(s1.substring(0,6));//returns va  
+System.out.print(s1.substring(3));//returns vatpoint
+System.out.print(s2);
+
+}
+
+}
