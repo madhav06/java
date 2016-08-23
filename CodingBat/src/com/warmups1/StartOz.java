00c11fbca3722887d394edd461e9a2955b9e13d7
Madhav Nandan <mnandan06@gmail.com>
2016-08-23 18:26:34 +0530

    Program 88
    CodingBat Warmups 01

---
+++ CodingBat/src/com/warmups1/StartOz.java
@@ -0,0 +1,33 @@
+package com.warmups1;
+
+public class StartOz {
+	
+	//Solution 1 
+	//Given a string, return a string made of the first 2 chars (if present), however include first char only if it is 'o' and include the second only if it is 'z', so "ozymandias" yields "oz".
+
+	
+	public String startOz(String str) {
+		  if(str.length()>=2 && str.substring(0,2).equals("oz")){return str.substring(0,2);}
+		  else if(str.length()>=1 && str.substring(0,1).equals("o")){return str.substring(0,1);}
+		  else if(str.length()>=2 && str.substring(1,2).equals("z")){return str.substring(1,2);}
+		  else{return str;}
+		}
+	
+	//Solution 2 illustrated by book
+	
+	//public String startOz(String str) {
+		 // String result = "";
+		  
+		  //if (str.length() >= 1 && str.charAt(0)=='o') {
+		  //  result = result + str.charAt(0);
+		  //}
+		  
+		 // if (str.length() >= 2 && str.charAt(1)=='z') {
+		   // result = result + str.charAt(1);
+		  //}
+		  
+		 // return result;
+		//}
+
+
+}
