27197919eacb4ead01c9f4400b13d07bafc33963
Madhav Nandan <mnandan06@gmail.com>
2016-08-23 17:36:19 +0530

    Program 83
    method describing return types according to the conditions
    
    /*Given two temperatures, return true if one is less than 0 and the other is greater than 100. */

---
+++ CodingBat/src/com/warmups1/IcyHot.java
@@ -0,0 +1,13 @@
+package com.warmups1;
+
+public class IcyHot {
+	
+	public boolean icyHot(int temp1, int temp2) {
+		  if((temp1<0 && temp2 >100) || (temp1 > 100 && temp2 <0)) {
+		    return true;
+		  }else{
+		    return false;
+		  }
+		}
+
+}
