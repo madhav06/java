3f7603f8a1e5079be9b7f3c1128af1d0691ea043
Madhav Nandan <mnandan06@gmail.com>
2016-08-26 18:53:12 +0530

    Program 93
    Array with counter

---
+++ JavaRevisited/src/com/javaloops/Apples.java
@@ -0,0 +1,18 @@
+package com.javaloops;
+
+public class Apples {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Index\tValue");
+		
+		int array[]= {32, 12, 18, 54, 02};
+		
+		for(int counter=0;counter<array.length;counter++){
+			
+			System.out.println(counter + "\t" + array[counter]);
+		}
+		
+	}
+
+}
