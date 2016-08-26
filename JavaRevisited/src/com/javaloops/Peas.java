e31d0787cf47e3ff761048cf6def30b59421b992
Madhav Nandan <mnandan06@gmail.com>
2016-08-26 18:54:36 +0530

    Program 94
    Array with operator

---
+++ JavaRevisited/src/com/javaloops/Peas.java
@@ -0,0 +1,17 @@
+package com.javaloops;
+
+public class Peas {
+	
+	public static void main(String[] args) {
+		
+		int ar[] = {3, 4, 5, 6, 7};
+		int total = 0;
+		
+		for(int x:ar){
+			
+			total +=x;
+		}
+		System.out.println(total);
+	}
+
+}
