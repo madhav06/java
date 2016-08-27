c7fd580781288585c1825f06c520592bb4bdfccd
Madhav Nandan <mnandan06@gmail.com>
2016-08-27 18:50:50 +0530

    Program 96
    Exception handling

---
+++ JavaRevisited/src/com/javaloops/Parent.java
@@ -0,0 +1,16 @@
+package com.javaloops;
+
+import java.io.*;  
+class Parent{  
+  void msg(){System.out.println("parent");}  
+}  
+  
+class TestExceptionChild1 extends Parent{  
+  void msg()throws ArithmeticException{  
+    System.out.println("child");  
+  }  
+  public static void main(String args[]){  
+   Parent p=new TestExceptionChild1();  
+   p.msg();  
+  }  
+}  
