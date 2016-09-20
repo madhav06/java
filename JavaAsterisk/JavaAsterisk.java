0883f04d28f114d868485f1a513e732a9b3a83f1
Madhav Nandan <mnandan06@gmail.com>
2016-09-20 22:23:01 +0530

    Program 104
    Print asterisk pattern

---
+++ JavaAsterisk/JavaAsterisk.java
@@ -0,0 +1,19 @@
+/* write a program to display pattern of asterisk */
+
+public class JavaAsterisk
+{
+
+  public static void main(String[] args){
+    printAsterisk();
+
+    //st
+  }
+  static void printAsterisk(){
+
+    System.out.println( "* * * * *" );
+    System.out.println( "* * * * *" );
+    System.out.println( "* * * * *" );
+    System.out.println( "* * * * *" );
+    System.out.println( "* * * * *" );
+  }
+}
