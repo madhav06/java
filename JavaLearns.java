f66724bfec8e362d90026769f2d097207dbacd44
Madhav Nandan <mnandan06@gmail.com>
2016-09-24 20:05:57 +0530

    Update JavaLearns.java

--- JavaLearns.java
+++ JavaLearns.java
@@ -1,15 +1,19 @@
+/*program to print random numbers 
+between integer 1 and 6
+*
+*/
 import java.util.*;
 public class JavaLearns {
   public static void main(String[] args)
   {
-    calculateValues();
+    calculateValues();//mrthod used to evaluate the random number
   }
 
   static void calculateValues(){
 
     int a;
-    Random rn = new Random();
-    a = 1+Math.abs(rn.nextInt()%6);
+    Random rn = new Random();//Constructor
+    a = 1+Math.abs(rn.nextInt()%6);//math library used
     System.out.print("The reult: "+a);
   }
 }
